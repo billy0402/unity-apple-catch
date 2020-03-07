65da32f9a1a61d57c6181dcb5037d28297207fd9
Billy Huang <10646003@ntub.edu.tw>
2020-03-08 03:29:13 +0800

    8-8 - 關卡設計

--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 using UnityEngine.UI; // 使用 UI 時必要的引用程式
 
 public class GameDirector : MonoBehaviour {
+    private GameObject _generator;
     private GameObject _timerText;
     private GameObject _pointText;
-    private float _time = 60.0f;
+    private float _time = 30.0f;
     private int _point = 0;
 
     // Start is called before the first frame update
     void Start() {
+        this._generator = GameObject.Find("ItemGenerator");
         this._timerText = GameObject.Find("Time");
         this._pointText = GameObject.Find("Point");
     }
@@ -18,6 +20,24 @@ public class GameDirector : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         this._time -= Time.deltaTime;
+
+        if (this._time < 0) {
+            this._time = 0;
+            this._generator.GetComponent<ItemGenerator>().SetParameter(10000.0f, 0, 0);
+        }
+        else if (0 <= this._time && this._time < 5) {
+            this._generator.GetComponent<ItemGenerator>().SetParameter(0.7f, -0.04f, 3);
+        }
+        else if (5 <= this._time && this._time < 12) {
+            this._generator.GetComponent<ItemGenerator>().SetParameter(0.5f, -0.05f, 6);
+        }
+        else if (12 <= this._time && this._time < 23) {
+            this._generator.GetComponent<ItemGenerator>().SetParameter(0.8f, -0.04f, 4);
+        }
+        else if (23 <= this._time && this._time < 30) {
+            this._generator.GetComponent<ItemGenerator>().SetParameter(1.0f, -0.03f, 2);
+        }
+
         this._timerText.GetComponent<Text>().text = this._time.ToString("F1");
         this._pointText.GetComponent<Text>().text = this._point.ToString() + " point";
     }
