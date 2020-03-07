341793c666b5a9400d536b192af51d384c0fc505
Billy Huang <10646003@ntub.edu.tw>
2020-03-08 02:44:53 +0800

    8-6 - 建立工廠

---
+++ Assets/Scripts/ItemGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour {
+    public GameObject applePrefab;
+    public GameObject bombPrefab;
+    private float _span = 1.0f;
+    private float _delta = 0;
+    private float _speed = -0.03f;
+    private int _ratio = 2;
+
+    public void SetParameter(float span, float speed, int ratio) {
+        this._span = span;
+        this._speed = speed;
+        this._ratio = ratio;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        this._delta += Time.deltaTime;
+        if (this._delta > this._span) {
+            this._delta = 0;
+
+            GameObject item;
+            int dice = Random.Range(1, 11);
+            if (dice <= this._ratio) {
+                item = Instantiate(bombPrefab) as GameObject;
+            }
+            else {
+                item = Instantiate(applePrefab) as GameObject;
+            }
+
+            float x = Random.Range(-1, 2);
+            float z = Random.Range(-1, 2);
+            item.transform.position = new Vector3(x, 4, z);
+
+            item.GetComponent<ItemController>().dropSpeed = this._speed;
+        }
+    }
+}
